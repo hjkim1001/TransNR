0d8de3130c0b06ce5bea7aef07907c004f20a7f7
hjkim1001 <168599408+hjkim1001@users.noreply.github.com>
2024-05-01 08:44:44 +0900

    Update Example_rankestimationsimul.R
    Fix errors

--- Codeexample/Example_rankestimationsimul.R
+++ Codeexample/Example_rankestimationsimul.R
@@ -2,14 +2,15 @@ path <- "C:/Users/USER/Desktop/Transferlearning/Function/"
 source(paste0(path,"Functions_rankestimation_simul.R")) # load r function
 
 
-##### Simulation model in Section 3.4 with n0=120, p=80, q=20, r=7, h=10
+
+##### Simulation model in Section 3.4 with n0=120, p=80, q=50, r=8, h=10
 
 ### Simulating 200 target data
 # rep: the number of replicates
 # n=n0, r=rank of B
 # corx: [Sigma_x]^{|i-j|}=corx^{|i-j|}
 # cory: [Sigma_epsilon]^{|i-j|}=Sigma_epsilon^{|i-j|}
-targestset <- generator_target_normal(rep=100,n=120,p=80,q=20,r=7, corx=0.5,cory=0)
+targestset <- generator_target_normal(rep=100,n=120,p=80,q=50,r=8, corx=0.5,cory=0)
 
 
 
@@ -17,10 +18,10 @@ targestset <- generator_target_normal(rep=100,n=120,p=80,q=20,r=7, corx=0.5,cory
 ## rank source= rank of contrast
 # corx: [Sigma_x]^{|i-j|}=corx^{|i-j|}
 # cory: [Sigma_epsilon]^{|i-j|}=Sigma_epsilon^{|i-j|}
-# generating 200 replicates for a source data with nk=300, transferring level=10, rank_source=5
-sources <- generator_source_rankestimationsimul(rep=200, n=300,p=80,q=20,ranksource =5,B=targestset$B,
-                                               corx=0.5,cory=0,
-                                               h=10,numsource=1)
+# generating 200 replicates for a source data with nk=300, transferring level=2, rank_source=5
+sources <-generator_source_rankestimationsimul(rep=200, n=150,p=80,q=50,B=targestset$B,
+                                                                              corx=0.5,cory=0,
+                                                                              h=2,numsource=1, ranksource =5)
 
 ### Fitting algorithms to the first five replicates
 
@@ -39,11 +40,12 @@ sources <- generator_source_rankestimationsimul(rep=200, n=300,p=80,q=20,ranksou
 # tol_inital: tolerance error in the ADMM algorithm for computing initial estimate
 # maxiter_biascorrection: the maxium of the number of iterations in the ADMM algorithm for computing contrast
 # tol_inital: tolerance error in the ADMM algorithm for computing contrast
+library(doParallel)
 cl <- makeCluster(3)
 registerDoParallel(cl)
 
-lamseq_w <-  c(0.01, 0.05, 0.1, 0.2, 0.3, 0.5, 0.6, 0.8, 0.9, 1, 1.2, 1.5, 2, 2.5, 3, 5, 7, 10, 12, 15,18,20,25, 30, 50)
-lamseq_delta <- c(lamseq_w,  100, 200, 500, 1000, 2000, 5000, 10000,20000)
+lamseq_w <-  c(0.1, 0.2, 0.3, 0.5, 0.6, 0.8, 0.9, 1, 1.2, 1.5, 2, 2.5, 3, 5, 7, 10, 12, 15,18,20,25, 30, 40, 50, 60, 70,100)
+lamseq_delta <- c(lamseq_w,  100, 200, 500, 1000, 2000, 5000, 7000, 10000, 15000, 20000)
 Repfit_TransNR_BIC(Ylist=targestset$Ylist,Xlist=targestset$Xlist,
                    auxYlist_list = sources$auxYlist_list, auxXlist_list=sources$auxXlist_list,
                    lamseq_w=lamseq_w, lamseq_delta=lamseq_delta, numsim=5,
@@ -62,7 +64,7 @@ Repfit_TransSCAD_BIC(Ylist=targestset$Ylist, Xlist=targestset$Xlist,
 
 rankvec_transSCAD <- unlist(lapply(res_transSCAD, function(x){ x$optrank_BIC }))
 rankvec_transNR <- unlist(lapply(res_transNR, function(x){ x$optrank_BIC }))
-c( sum(rankvec_transSCAD==7), sum(rankvec_transNR==7 ) )/5 
+c( sum(rankvec_transSCAD==8), sum(rankvec_transNR==8 ) )/5 
 ### rank 
 # first row: average of ranks for Trans-SCAD, standard deviation of ranks for Trans-SCAD 
 # second row: average of ranks for Trans-NR, standard deviation of ranks for Trans-NR 
@@ -75,7 +77,7 @@ round( rbind(c( mean(rankvec_transSCAD), sd(rankvec_transSCAD)),
 # second row: average of EEs for Trans-NR, standard deviation of EEs for Trans-NR 
 
 Errorvec_transSCAD <- unlist(lapply(res_transSCAD, function(x){ sqrt( mean( (x$B[-1,]- targestset$B)^2 )) }))
-Errorvec_transNR <- unlist(lapply(Errorvec_transNR, function(x){ sqrt( mean( (x$B[-1,]- targestset$B)^2 )) }))
+Errorvec_transNR <- unlist(lapply(res_transNR, function(x){ sqrt( mean( (x$B[-1,]- targestset$B)^2 )) }))
 
 round( rbind(c( mean(Errorvec_transSCAD), sd(Errorvec_transSCAD)),
               c(  mean(Errorvec_transNR), sd(Errorvec_transNR)) ), 3)
